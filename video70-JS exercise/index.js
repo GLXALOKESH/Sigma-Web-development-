bde1bf9d7f239309161dabac2944a2415baefd6d
GLXALOKESH <alokeshmaitra9@gmail.com>
2024-04-01 13:32:45 +0530

    video70

---
+++ video70-JS exercise/index.js
@@ -0,0 +1,6 @@
+let cont = document.body.getElementsByClassName("container")[0].children 
+cont[0].style.backgroundColor = "red"
+cont[1].style.backgroundColor = "green"
+cont[2].style.backgroundColor = "blue"
+cont[3].style.backgroundColor = "yellow"
+cont[4].style.backgroundColor = "cyan"
