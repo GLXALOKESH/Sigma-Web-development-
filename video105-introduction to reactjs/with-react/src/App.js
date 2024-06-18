211ec2c64681285f908f19ec1b2568b0a52bdc69
GLXALOKESH <alokeshmaitra9@gmail.com>
2024-06-19 01:19:33 +0530

    video 105 completed

---
+++ video105-introduction to reactjs/with-react/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import Navbar from './components/navbar';
+function App() {
+  const [value, setValue] = useState(0);
+  
+  return (
+    <>
+      <Navbar /> 
+      <div className="App">
+        <div className="value">{value}</div>
+        <button onClick={() => { setValue(value + 1); }}>Click Me</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ video105-introduction to reactjs/with-react/src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      <div className="vav">
+        <ul>
+            <li>home</li>
+            <li>about</li>
+            <li>contat us</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default navbar
